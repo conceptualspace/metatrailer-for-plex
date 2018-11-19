d11c8b2ce8111d280745d58ef7deee526074e547
conceptualspace <dev@conceptualspace.net>
2018-11-18 22:19:03 -0500

    improve injection method
    > listen for the specific selector instead of trying to detect page loads (arrive.js)
    > fixed the selector for movie year (previous verison was not correct)
    > bump to v1.1

--- src/content_script.js
+++ src/content_script.js
@@ -1,22 +1,4 @@
 
-let currentPage = '';
-
-function insertTrailer() {
-
-    // selectors subject to change.. working as of nov 15 2018
-    let title = $("div[data-qa-id='preplayMainTitle']")[0].textContent;
-    let year = $("span[data-qa-id='metadataYear']")[0].textContent;
-
-    let trailer = document.createElement('a');
-    trailer.setAttribute('href',"https://www.youtube.com/results?search_query="+title+"+"+year+"+trailer");
-    trailer.setAttribute('target',"_blank");
-    trailer.innerHTML = "Play Trailer";
-
-    $('.PrePlayMetadataInnerContent-innerContent-1BPzw')[0].appendChild(trailer);
-
-
-}
-
 /*
 // todo: embed trailer in an iframe
 $.get( "https://www.youtube.com/results?search_query=movie+trailer", function( data ) {
@@ -24,11 +6,17 @@ $.get( "https://www.youtube.com/results?search_query=movie+trailer", function( d
 });
 */
 
-// detect page change
-(document.body).addEventListener('transitionend', function(event) {
-    if (location.href !== currentPage) {
-        currentPage = location.href;
-        // plex does some weird dom updating stuff, delay a bit to make sure elements are ready
-        setTimeout(insertTrailer, 500);
+$(document).arrive("div[data-qa-id='preplaySecondTitle']", function() {
+    // selectors subject to change; working with web client v3.75.3
+    let title = $("div[data-qa-id='preplayMainTitle']")[0].textContent;
+    let year = $("div[data-qa-id='preplaySecondTitle']")[0].textContent;
+
+    if (! $('#trailer')[0]) {
+        let trailer = document.createElement('a');
+        trailer.setAttribute("id", "trailer");
+        trailer.setAttribute('href',"https://www.youtube.com/results?search_query="+title+"+"+year+"+trailer");
+        trailer.setAttribute('target',"_blank");
+        trailer.innerHTML = "Play Trailer";
+        $('.PrePlayMetadataInnerContent-innerContent-1BPzw')[0].appendChild(trailer);
     }
-}, true);
+});
