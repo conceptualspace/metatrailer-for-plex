c696fce8f24267a961a68e0a83222e24a6f6c0fb
conceptualspace <dev@conceptualspace.net>
2019-02-27 23:39:01 -0500

    support new plex element ids

--- src/content_script.js
+++ src/content_script.js
@@ -9,7 +9,7 @@ $.get( "https://www.youtube.com/results?search_query=movie+trailer", function( d
 $(document).arrive("div[data-qa-id='preplaySecondTitle']", function() {
     // selectors subject to change; working with web client v3.75.3
     let title = $("div[data-qa-id='preplayMainTitle']")[0].textContent;
-    let year = $("div[data-qa-id='preplaySecondTitle'] .PrePlayLeftTitle-leftTitle-Ev1KG")[0].textContent;
+    let year = $("div[data-qa-id='preplaySecondTitle'] .PrePlayLeftTitle-leftTitle-Ev1KGW")[0].textContent;
 
     if (! $('#trailer')[0]) {
         let trailer = document.createElement('a');
@@ -17,6 +17,6 @@ $(document).arrive("div[data-qa-id='preplaySecondTitle']", function() {
         trailer.setAttribute('href',"https://www.youtube.com/results?search_query="+title+"+"+year+"+trailer");
         trailer.setAttribute('target',"_blank");
         trailer.innerHTML = "Play Trailer";
-        $('.PrePlayMetadataInnerContent-innerContent-1BPzw')[0].appendChild(trailer);
+        $('.PrePlayMetadataInnerContent-innerContent-1BPzwp')[0].appendChild(trailer);
     }
 });
