0199d97273c5dd00dee1d0c593519dab391fa4b6
conceptualspace <dev@conceptualspace.net>
2020-07-19 00:25:28 -0400

    support plex v4
    - new selectors for v4.38.x
    - maintain compatibility with v3.x
    - update jquery
    - request broader host permissions to support plex on custom domains

--- src/content_script.js
+++ src/content_script.js
@@ -1,22 +1,44 @@
+// metatrailer for plex
+// copyright dev@conceptualspace.net
+// absolutely no warranty is expressed or implied
+
+'use strict';
 
 /*
-// todo: embed trailer in an iframe
+// todo: embed trailer?
 $.get( "https://www.youtube.com/results?search_query=movie+trailer", function( data ) {
     $( ".result" ).html( data );
 });
 */
 
+// todo remove jquery dependency
+
+function createTrailer(title, year, margin) {
+    let trailer = document.createElement('a');
+    trailer.setAttribute("id", "trailer");
+    trailer.setAttribute('href',"https://www.youtube.com/results?search_query="+title+"+"+year+"+trailer");
+    trailer.setAttribute('target',"_blank");
+    if (margin) {
+        trailer.style.marginLeft = '20px';
+    }
+    trailer.innerText = "Play Trailer";
+    return trailer;
+}
+
+// selectors for v3.x
 $(document).arrive("div[data-qa-id='preplaySecondTitle']", function() {
-    // selectors subject to change; working with web client v3.75.3
     let title = $("div[data-qa-id='preplayMainTitle']")[0].textContent;
     let year = $("div[data-qa-id='preplaySecondTitle'] .PrePlayLeftTitle-leftTitle-Ev1KGW")[0].textContent;
-
     if (! $('#trailer')[0]) {
-        let trailer = document.createElement('a');
-        trailer.setAttribute("id", "trailer");
-        trailer.setAttribute('href',"https://www.youtube.com/results?search_query="+title+"+"+year+"+trailer");
-        trailer.setAttribute('target',"_blank");
-        trailer.innerHTML = "Play Trailer";
-        $('.PrePlayMetadataInnerContent-innerContent-1BPzwp')[0].appendChild(trailer);
+        $('.PrePlayMetadataInnerContent-innerContent-1BPzwp')[0].appendChild(createTrailer(title, year, false));
+    }
+});
+
+// selectors for v4.x
+$(document).arrive("div[data-qa-id='preplay-secondTitle']", function() {
+    let title = $("div[data-qa-id='preplay-mainTitle']")[0].textContent;
+    let year = $("div[data-qa-id='preplay-secondTitle']")[0].textContent;
+    if (! $('#trailer')[0]) {
+        $('.PrePlayTertiaryTitle-tertiaryTitle-1LwUaC')[0].appendChild(createTrailer(title, year, true));
     }
 });
